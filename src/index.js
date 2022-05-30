972574b3b2282f6970e6771fbc1a41577428cb25
zrbrooks <103884081+zrbrooks@users.noreply.github.com>
2022-05-30 22:29:34 +0100

    Changed the Date function
    date function now is timestamp

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 10) {
+if (hours < 10) {
     hours = `0${hours}`;
-  }
+}
   let minutes = date.getMinutes();
+
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
-  let dayIndex = date.getDay();
 
   let days = [
     "Sunday",
@@ -19,14 +19,15 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[dayIndex];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
 
 function displayWeatherCondition(response) {
-
   document.querySelector("#city").innerHTML = response.data.name;
+  let dateElement = document.querySelector("#date");
+
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
@@ -36,10 +37,12 @@ function displayWeatherCondition(response) {
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
-    document.querySelector("#sunrise").innerHTML = response.data.sys.sunrise;
-      document.querySelector("#sunset").innerHTML = response.data.sys.sunset;
 
+  document.querySelector("#sunrise").innerHTML = response.data.sys.sunrise;
+  document.querySelector("#sunset").innerHTML = response.data.sys.sunset;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
+
 
 function search(event) {
   event.preventDefault();
@@ -49,29 +52,20 @@ function search(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeatherCondition);
-
 }
 
 function showPosition(position) {
-let button = document.querySelector("button");
-button.innerHTML = `${position.coords.latitude} and ${position.coords.longitude}`;
-
-
+  let button = document.querySelector("button");
+  button.innerHTML = `${position.coords.latitude} and ${position.coords.longitude}`;
 }
 
-function getCurrentPosition(position) {
-
-}
+function getCurrentPosition(position) {}
 navigator.geolocation.getCurrentPosition(showPosition);
 
 let button = document.querySelector("button");
 button = addEventListener("click", getCurrentPosition);
 
 
-
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
 
 let row = document.querySelector("#search-row");
 row.addEventListener("submit", search);
@@ -94,5 +88,3 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
-
-
